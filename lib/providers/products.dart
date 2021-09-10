3bc40919feae75065fc3731bacb2d75e1a686fe9
babyazalea <usun12@hotmail.com>
2021-09-10 14:21:04 +0900

    error handling with async-await

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -142,21 +142,19 @@ class Products with ChangeNotifier {
     }
   }
 
-  void deleteProduct(String id) {
-    final url = Uri.https(
-        'vue-http-demo-c1945-default-rtdb.firebaseio.com', '/products/$id.jso');
+  Future<void> deleteProduct(String id) async {
+    final url = Uri.https('vue-http-demo-c1945-default-rtdb.firebaseio.com',
+        '/products/$id.json');
     final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
     var existingProduct = _items[existingProductIndex];
-    http.delete(url).then((response) {
-      if (response.statusCode >= 400) {
-        throw HttpException('Could not delete product.');
-      }
-      existingProduct = null;
-    }).catchError((_) {
-      _items.insert(existingProductIndex, existingProduct);
-      notifyListeners();
-    });
     _items.removeAt(existingProductIndex);
     notifyListeners();
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException('Could not delete product.');
+    }
+    existingProduct = null;
   }
 }
